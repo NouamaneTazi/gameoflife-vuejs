b4d154a68f92d2727297c9f7a9bec4a66e0f9b9f
Nouamane Tazi <nouamane98@gmail.com>
2020-05-16 15:12:54 +0200

    Added general Layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Grid />
+    <Controls />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Grid from "./components/Grid.vue";
+import Controls from "./components/Controls.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header,
+    Grid,
+    Controls
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Controls.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{prop}}</div>
+</template>
+
+<script>
+export default {
+    name: "Template",
+    props: ['prop']
+}
+</script>
+
+<style scoped>
+
+</style>
